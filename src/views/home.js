8b40378f7b9b5d5519eeb4632f296fdf2914dc9b
Chris00Robert <chris00robert@gmail.com>
2021-03-14 13:04:02 -0600

    Cambio2

--- src/views/home.js
+++ src/views/home.js
@@ -1,10 +1,41 @@
 import React from 'react'
+import {ViewContent, Icon, Text, Button, Input, } from "../components/viewContent"
 import { View, Text } from 'react-native'
 const Home = () =>{
     return (
-        <View>
-          <Text>Home</Text>
-        </View>
+      <ViewContent style = {EstiloPE.maquetado}>
+      <ViewContent>
+          <TouchableOpacity onPress = {()=>alert('Estamos trabajando')}></TouchableOpacity>
+          <Text>Nombre App</Text>
+          <TouchableOpacity></TouchableOpacity>
+      </ViewContent>
+      
+      <ScrollView>
+        <ViewContent style = {EstiloE.post}>
+            <Icon></Icon>
+            <Text></Text>
+            <Text></Text>
+            <ViewContent></ViewContent>
+        </ViewContent>
+      </ScrollView>
+
+      <Touchable style = {EstiloE.btnPost}>
+        <Icon></Icon>
+      </Touchable>
+  </ViewContent>
     )
 };
 export default Home
+
+const EstiloE = StyleSheet.create({
+  maquetado: {
+      display:"flex",
+      flexDirection:"row"
+  },
+  btnPost:{
+      position:"absolute"
+  },
+  post:{
+      position:"relative"
+  }
+})
